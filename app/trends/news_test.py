1285e492e7c85b2c62bb9acf8580b32de29bffab
Patrick595 <patrickbuong@gmail.com>
2020-02-02 16:05:21 +0300

    created a function that will initiate our news to make the trending news

--- app/trends/news_test.py
+++ app/trends/news_test.py
@@ -2,6 +2,21 @@ import unittest
 from trends import news
 news = news.news
 
+class newsTest(unittest.TestCase):
+    '''
+    Test Class to test the behaviour of the news class
+    '''
+
+    def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
+        # self.new_movie = Movie(1234,'Python Must Be Crazy','A thrilling new Python Series','https://image.tmdb.org/t/p/w500/khsjha27hbs',8.5,129993)
+
+    def test_instance(self):import unittest
+from trends import news
+news = news.news
+
 
 class newsTest(unittest.TestCase):
     '''
@@ -11,3 +26,8 @@ class newsTest(unittest.TestCase):
 
     def setUp(self):
         
+        self.assertTrue(isinstance(self.trending_news,news))
+
+
+if __name__ == '__main__':
+    unittest.main()
