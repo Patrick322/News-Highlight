1b8e99e98dc2a7ed03a5574981493da92eaf070c
Patrick595 <patrickbuong@gmail.com>
2020-02-03 10:54:50 +0300

    created a function that will get response of news from json to our url request

--- app/requests.py
+++ app/requests.py
@@ -1,8 +1,17 @@
 from app import app
 import urllib.request,json
-form .
+form .trends import news
+
+News = news.news
+
+
 # Getting api key
 api_key = app.config['NEWS_API_KEY']
 
 # Getting the movie base url
 base_url = app.config["NEWS_API_BASE_URL"]
+
+
+def get_news(category):
+    '''
+    Function that gets the json response to our url request
